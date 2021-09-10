ab9b400bea02a2f374f65f039d4789628b517160
tusheet <tgoli@sametunes.com>
2021-09-10 02:19:43 -0400

    not working: validation

--- app/src/main/java/com/example/tictactoe/LoginPlayer1.java
+++ app/src/main/java/com/example/tictactoe/LoginPlayer1.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 public class LoginPlayer1 extends AppCompatActivity {
 
@@ -39,15 +44,48 @@ public class LoginPlayer1 extends AppCompatActivity {
     }
 
     private void validate(String name, String pass) {
-        if(true) {
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference db = database.getReference("users");
-            User user = new User(name, pass, 0, 0);
-            db.child(name).setValue(user);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference db = database.getReference("users");
 
-            Intent intent = new Intent(LoginPlayer1.this, LoginPlayer2.class);
-            startActivity(intent);
+        if (!name.isEmpty() && !pass.isEmpty()) {
+            db.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot data: dataSnapshot.getChildren()){
+                        if (data.child(name).exists()) {
+                            String password = data.child(name).child("password").toString();
+                            Log.e("LoginPlayer1", password);
+
+                            if (password.equals(pass)) {
+                                Intent intent = new Intent(LoginPlayer1.this, LoginPlayer2.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(LoginPlayer1.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                                ctr--;
+                                tv.setText("Number Of Attempts Remaining : " + ctr);
+                                if (ctr == 0) {
+                                    ctr = 5;
+                                    bt.setEnabled(false);
+                                    Intent it = new Intent(LoginPlayer1.this, StartScreen.class);
+                                    startActivity(it);
+                                }
+                            }
+                        } else {
+                            User user = new User(name, pass, 0, 0);
+                            db.child(name).setValue(user);
+                            Intent intent = new Intent(LoginPlayer1.this, LoginPlayer2.class);
+                            startActivity(intent);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError firebaseError) {
+                    Toast.makeText(LoginPlayer1.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            });
         } else {
+            Toast.makeText(LoginPlayer1.this, "Illegal Username/Password", Toast.LENGTH_SHORT).show();
             ctr--;
             tv.setText("Number Of Attempts Remaining : " + ctr);
             if (ctr == 0) {
