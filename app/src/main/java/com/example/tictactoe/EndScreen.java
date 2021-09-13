3b9d66d99cb45e9b5b4d3fd1b1074076bc052e89
tusheet <tgoli@sametunes.com>
2021-09-12 22:55:23 -0400

    removed looping end screen

--- app/src/main/java/com/example/tictactoe/EndScreen.java
+++ app/src/main/java/com/example/tictactoe/EndScreen.java
@@ -10,19 +10,5 @@ public class EndScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    sleep(5000);
-                } catch (Exception e){
-                    e.printStackTrace();
-                } finally {
-                    Intent intent = new Intent(EndScreen.this, StartScreen.class);
-                    startActivity(intent);
-                }
-            }
-        };
-        thread.start();
     }
 }
